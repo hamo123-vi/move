4c690685bd4efd206838c5cb1c0f69151f14a67e
hamova <amil.valjevac@stu.ibu.edu.ba>
2021-03-28 12:51:30 +0200

    Developed get_diagnosis_by_user_id function

--- api/dao/DiagnosisCheckDao.Class.php
+++ api/dao/DiagnosisCheckDao.Class.php
@@ -5,7 +5,20 @@ class DiagnosisCheckDao extends BaseDao
     public function get_diagnosis_by_examination_id($examination_id)
     {
         return $this->query("SELECT a.type FROM diagnosis a
-                     JOIN diagnosis_check b ON a.id=b.diagnosis_id
-                     JOIN examinations c ON c.id=b.examination_id",["b.examination_id" => $examination_id]);
+                            JOIN diagnosis_check b ON a.id=b.diagnosis_id
+                            JOIN examinations c ON c.id=b.examination_id",
+                            ["b.examination_id" => $examination_id]
+                            );
+    }
+
+    public function get_diagnosis_by_user_id($user_id)
+    {
+        return $this->query("SELECT a.type FROM diagnosis a
+                            JOIN diagnosis_check b ON a.id=b.diagnosis_id
+                            JOIN examinations c ON b.examination_id=c.id
+                            JOIN appointments d ON c.appointment_id=d.id
+                            JOIN users e ON d.user_id=e.id",
+                            ["e.id" => $user_id]
+                            );
     }
 }
