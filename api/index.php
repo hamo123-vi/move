e47d0dec47c2ca99e562e05e984c234cae4444a8
hamova <amil.valjevac@stu.ibu.edu.ba>
2021-03-29 18:53:03 +0200

    POST user Route

--- api/index.php
+++ api/index.php
@@ -1,13 +1,32 @@
 <?php
 
-require_once dirname(__FILE__)."/dao/BaseDao.Class.php";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require dirname(__FILE__)."/dao/UserDao.Class.php";
 require dirname(__FILE__).'/../vendor/autoload.php';
 
-$valjevac=new BaseDao();
+Flight::route('GET /users',function(){
+    $dao=new UserDao();
+    $users=$dao->get_all_users();
+    Flight::json($users);
+});
+
+Flight::route('GET /user/@id',function($id){
+    $dao=new UserDao();
+    $user=$dao->get_user_by_id($id);
+    Flight::json($user);
+});
+
+Flight::route('POST /user', function(){
+    $request=Flight::request();
+    $data=$request->data->getData();
+    $dao=new UserDao();
+    $dao->add_user($data);
+    Flight::json($data);
+});
 
 
 
-
-
-
-?>
+Flight::start();
