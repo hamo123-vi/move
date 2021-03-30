772b5d4d6c41e744c6c95a962e8d83eb9078de53
hamova <amil.valjevac@stu.ibu.edu.ba>
2021-03-30 14:44:29 +0200

    Reorganization Routes

---
+++ api/routes/users_routes.php
@@ -0,0 +1,31 @@
+<?php
+
+Flight::route('GET /users',function(){
+    $dao=new UserDao();
+    $users=$dao->get_all_users();
+    Flight::json($users);
+});
+
+Flight::route('GET /user/@id',function($id){
+    $dao=new UserDao();
+    $user=$dao->get_user_by_id($id);
+    Flight::json($user);
+});
+
+Flight::route('POST /user', function(){
+    $request=Flight::request();
+    $data=$request->data->getData();
+    $dao=new UserDao();
+    $dao->add_user($data);
+    Flight::json($data);
+});
+
+Flight::route('POST /update_user/@id', function($id){
+    $dao=new UserDao();
+    $data=Flight::request()->data->getData();
+    $dao->update_user_by_id($id, $data);
+    Flight::json($data);
+});
+
+
+Flight::start();
