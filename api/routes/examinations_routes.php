6a741fbe2535e5a161eafbe5bfc3378212a3aa46
hamova <amil.valjevac@stu.ibu.edu.ba>
2021-04-07 19:59:57 +0200

    Business Logic and Routes layer completed for table 'examiantions'

--- api/routes/examinations_routes.php
+++ api/routes/examinations_routes.php
@@ -3,3 +3,9 @@
 Flight::route('GET /examinations', function(){
     Flight::json(Flight::examinationService()->get_examinations());
 });
+
+Flight::route('POST /examination', function(){
+    $data=Flight::request()->data->getData();
+    Flight::examinationService()->insert_examination($data);
+    Flight::json($data);
+});
